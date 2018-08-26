f43f4902741ce67b03559ecc7f7cc63d8b3a6cd1
Ken Murata <k_murata@nttpc.co.jp>
2018-08-26 05:09:58 +0000

    auto create balance_class 2

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -2,8 +2,12 @@ class Balance < ActiveRecord::Base
   validates :column, :value, :balance_class,  presence: true
   validates :value, numericality: true
   
+  before_save :balance_class_find_or_create
   # 分類が存在しなければ登録する(Lineから登録される場合を想定)
-  BalanceClass.find_or_create_by(:balance_class => :balance_class) do |bc|
-    bc.balance_type = :balance_type
+  def balance_class_find_or_create
+    balanceclass = self.balance_class
+    BalanceClass.find_or_create_by(:balance_class => balanceclass) do |bc|
+      bc.balance_type = self.balance_type
+    end
   end
 end
