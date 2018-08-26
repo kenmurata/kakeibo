c361547f9e6b5f63dbbba83e16908157a41e5136
Ken Murata <k_murata@nttpc.co.jp>
2018-08-26 04:54:28 +0000

    add auto create balanceclass

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,2 +1,9 @@
 class Balance < ActiveRecord::Base
+  validates :column, :value, :balance_class,  presence: true
+  validates :value, numericality: true
+  
+  # 分類が存在しなければ登録する(Lineから登録される場合を想定)
+  BalanceClass.find_or_create_by(:balance_class => :balance_class) do |bc|
+    bc.balance_type = :balance_type
+  end
 end
